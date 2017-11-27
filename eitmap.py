93b45f9b0bad7b7732659c265c2e61c35d7a58ae
root <root@localhost.localdomain>
2017-11-27 10:58:23 +0100

    EIT new files (map / results / spelling correction/ sparse matrix of all data)

---
+++ eitmap.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 13:14:36 2017
+
+@author: ismailerradi
+"""
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier as rf
+from scipy.sparse import *
+from sklearn.svm import LinearSVC as LSVC
+import pandas as pd
+import numpy as np
+import time
+import unicodedata as ud
+import nltk as n
+import enchant
+from spellcheck import SpellingReplacer
+
+columns = ['docID', 'year', 'gender', 'age', 'lod', 'lineID', 'rawText', 'intT', 'intV', 'Cause', 'stdText', 'ICD10']
+DEL = ['docID', 'year', 'gender', 'age', 'lod', 'lineID', 'intT', 'intV', 'Cause', 'stdText']
+
+
+def save_sparse(filename, array):
+    np.savez(filename,data = array.data ,indices=array.indices,
+             indptr =array.indptr, shape=array.shape )
+
+def load_sparse(filename):
+    loader = np.load(filename)
+    return csc_matrix(( loader['data'], loader['indices'], loader['indptr']),
+                         shape = loader['shape'])
+
+
+def normalize(s):
+    s = ud.normalize('NFKD', s).encode('ascii', 'ignore').decode()
+    return s.lower()
+
+def tokens(line):
+    words= n.RegexpTokenizer(r'\w+').tokenize(line)
+    for i in range(len(words)):
+        words[i] = correction.replace(words[i])
+    return words
+        
+
+def fill_map(df):
+    hashmap = {}
+    j = 0
+    for i,d in enumerate(df['rawText']):
+        words = tokens(d)
+        for k in words:
+            if k not in hashmap:
+                hashmap[k] = j
+                j = j+1
+    return hashmap
+
+
+def model_class(set):
+    df = set['rawText']
+    x = []
+    y = []
+    ones = []
+    for i in range(len(set)):
+        words = tokens(df[i])
+        for k in words:
+            x.append(i)
+            y.append(cols.index(k))
+            ones.append(1)
+    x = np.asarray(x)
+    y = np.asarray(y)
+    ones = np.asarray(ones)
+    X = csc_matrix( (ones, (x, y)), shape=(len(set), len(cols)))
+    return X
+
+
+data = pd.read_csv(
+    '/root/Bureau/EIT/corpus/train/train.csv',
+    skipinitialspace=True,
+    sep=';',
+    names=columns)
+
+dev = pd.read_csv(
+    '/root/Bureau/EIT/corpus/dev/dev_full.csv',
+    skipinitialspace=True,
+    sep=';',
+    names=columns)
+
+data = data.dropna(subset=['ICD10'])
+data = data.reset_index(drop=True)
+dev = dev.dropna(subset=['ICD10'])
+dev = dev.reset_index(drop=True)
+
+for i in DEL:
+    data.drop(i, axis=1, inplace=True)
+    dev.drop(i, axis=1, inplace=True)
+
+data = data[:15000]
+dev = dev[:15000]
+
+data['rawText'] = data['rawText'].apply(normalize)
+dev['rawText'] = dev['rawText'].apply(normalize)
+
+correction = SpellingReplacer()
+hashmap = fill_map(pd.concat([data, dev], ignore_index=True))
+cols = list(hashmap.keys())
+print(cols)
+print(len(cols))
+"""trainX = model_class(data)
+evalX = model_class(dev)
+save_sparse('/root/Bureau/train15C', trainX)
+save_sparse('/root/Bureau/eval15C', evalX)
+
+trainX = load_sparse('/root/Bureau/train15.npz')
+evalX = load_sparse('/root/Bureau/eval15.npz')
+trainY = data['ICD10'].values
+evalY = dev['ICD10'].values
+print("trainX :", trainX.shape)
+print("trainY :", trainY.shape)
+print("evalX :", evalX.shape)
+print("evalY :", evalY.shape)
+
+models = []
+models.append(('rf', rf(n_estimators=10)))
+models.append(('LSVC', LSVC(random_state=0)))
+
+
+for name, model in models:
+    start_time = time.time()
+    model.fit(trainX, trainY)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print(name)
+    print('Score for evaluation set :', model.score(evalX, evalY))
+"""
+
